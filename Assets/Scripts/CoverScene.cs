016dfac14aa88f8a5feb41f57e004c48dc6103e3
Yukii <yukiizzzx@gmail.com>
2018-12-12 07:01:48 +0800

    -

--- Assets/Scripts/CoverScene.cs
+++ Assets/Scripts/CoverScene.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CoverScene : MonoBehaviour {
-
+public class CoverScene : MonoBehaviour
+{
+	private bool fade;
+	private GameObject fadeObj;
+	public float fadeSpeed = 0.5f;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,7 +17,19 @@ public class CoverScene : MonoBehaviour {
 	void Update () {
 		if (Input.anyKeyDown)
 		{
-			SceneManager.LoadScene(1);
+			fade = true;
+		}
+
+		if (fade)
+		{
+			fadeObj.GetComponent<SpriteRenderer>().color = new Color(fadeObj.GetComponent<SpriteRenderer>().color.r,
+				fadeObj.GetComponent<SpriteRenderer>().color.b,
+				fadeObj.GetComponent<SpriteRenderer>().color.g,
+				fadeObj.GetComponent<SpriteRenderer>().color.a * Mathf.Lerp(fadeObj.GetComponent<SpriteRenderer>().color.a,1f,fadeSpeed * Time.deltaTime));
+			if (fadeObj.GetComponent<SpriteRenderer>().color.a < 0.01f)
+			{
+				SceneManager.LoadScene(1);
+			}
 		}
 	}
 }
